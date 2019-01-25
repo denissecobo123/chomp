db3cf06c7983f1902a99c689a7719acd73257d30
Mr. Enrico <denrico@northbergen.k12.nj.us>
2019-01-25 11:25:48 -0500

    Flesh out *some* of the methods from ChompGame. Huge gaps remain!

--- chomp.py
+++ chomp.py
@@ -1,15 +1,46 @@
 import numpy as np
 import pandas as pd
+import random
 
 EMOJI = {-1: '\u2612', 0: ' ', 1: '\u2610'}
 
 
 class ChompGame:
-    def __init__(self):
-        pass
+    """Contains the control flow for the game"""
+    def __init__(self, n_players=2, size=(3, 4)):
+        self.n_players = n_players
+        self.size = size
+        self.board = Board(*size)
+        self.game_over = False
+        self.players = []
+        self.current_player = None
 
     def __repr__(self):
-        pass
+        return f'ChompGame({self.n_players}, {self.size})'
+
+    def play(self):
+        self.setup()
+        while not self.game_over:
+            print(f'{self.current_player}, it\'s your turn!\n')
+            print(self.board)
+            self.move()
+            if self.board.state[-1][0] == 0:
+                self.game_over = True
+                self.current_player.wins += 1
+
+    def setup(self):
+        for i in range(1, self.n_players + 1):
+            print(f'***Player {i}***')
+            self.players.append(Player())
+
+        self.current_player = random.choice(self.players)
+
+    def move(self):
+        coord_str = input("Enter the coordinates for your move. (e.g. A3)")
+        row_str, col_str = coord_str[0].upper(), coord_str[1]
+        row = ord(row_str) - 65
+        col = int(col_str)
+        self.board.take(row, col)
 
 
 class Board:
@@ -32,12 +63,19 @@ class Board:
         return str(board_df)
 
     def take(self, row, col):
-        pass
+        # self.state[:row+1, col:] = 0
+        for r in range(row+1):
+            self.state[r][col:] = 0
 
-    
+
 class Player:
     def __init__(self):
-        pass
+        self.name = input("\tEnter your name: ")
+        self.wins = 0
 
     def __repr__(self):
-        pass
+        return f'Player({self.name})'
+
+    def __str__(self):
+        return self.name
+
